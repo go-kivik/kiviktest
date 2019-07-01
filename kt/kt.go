3ba4b4a49699ff9dc48de39c6b2a0009f25b9824
Jonathan Hall <flimzy@flimzy.com>
2019-07-01 22:09:28 +0200

    Safer temp db names

--- kt/kt.go
+++ kt/kt.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"io/ioutil"
 	"math/rand"
+	"regexp"
 	"strings"
 	"sync"
 	"testing"
@@ -211,10 +212,13 @@ func (c *Context) TestDBName() string {
 	return TestDBName(c.T)
 }
 
+var invalidDBCharsRE = regexp.MustCompile(`[^a-z0-9_$\(\)+/-]`)
+
 // TestDBName generates a randomized string suitable for a database name for
 // testing.
 func TestDBName(t *testing.T) string {
 	id := strings.ToLower(tName(t))
+	id = invalidDBCharsRE.ReplaceAllString(id, "_")
 	id = id[strings.Index(id, "/")+1:]
 	id = strings.Replace(id, "/", "_", -1) + "$"
 	rndMU.Lock()
