8fb3b4346c7ca5130ca1ebb636db43a3934f6149
Jonathan Hall <flimzy@flimzy.com>
2020-10-19 22:09:24 +0200

    Pass the proper database name during replicaiton test setup

--- client/replicate.go
+++ client/replicate.go
@@ -45,7 +45,7 @@ func testReplication(ctx *kt.Context, client *kivik.Client) {
 	defer ctx.DestroyDB(dbtarget)
 	defer ctx.DestroyDB(dbsource)
 
-	db := ctx.Admin.DB(context.Background(), dbsource)
+	db := ctx.Admin.DB(context.Background(), ctx.TestDB())
 	if err := db.Err(); err != nil {
 		ctx.Fatalf("Failed to open db: %s", err)
 	}
